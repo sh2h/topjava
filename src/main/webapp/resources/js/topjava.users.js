7b28410f20e25fa7780d3db889e7605aefa70b8c
Dmitry Viktorov <sh5h@yandex.ru>
2019-05-27 22:10:56 +0300

    9 01 HW8

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -36,7 +36,10 @@ $(function () {
                         "asc"
                     ]
                 ]
-            })
+            }),
+            updateTable: function () {
+                $.get(userAjaxUrl, updateTableByData);
+            }
         }
     );
 });
